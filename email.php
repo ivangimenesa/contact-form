49d7b43019d067f9cb03f3f2b2fbd5ef0ab8900a
ivangimenesa <igntemplates@gmail.com>
2017-05-06 14:04:49 -0300

    Update email.php

--- email.php
+++ email.php
@@ -1,49 +1,47 @@
 <?php
-$Nome		= $_POST["Nome"];	// Pega o valor do campo Nome
-$Fone		= $_POST["Fone"];	// Pega o valor do campo Telefone
-$Email		= $_POST["Email"];	// Pega o valor do campo Email
-$Mensagem	= $_POST["Mensagem"];	// Pega os valores do campo Mensagem
+$nome   = $_POST["nome"]; // Pega o valor do campo Nome
+$fone   = $_POST["fone"]; // Pega o valor do campo Telefone
+$email    = $_POST["email"];  // Pega o valor do campo Email
+$estado = $_POST["estado"]; // Pega o valor do campo estado
+$mensagem = $_POST["mensagem"]; // Pega os valores do campo Mensagem
 
 // Variável que junta os valores acima e monta o corpo do email
 
-$Vai 		= "Nome: $Nome\n\nE-mail: $Email\n\nTelefone: $Fone\n\nMensagem: $Mensagem\n";
+$Vai    = "Nome: $nome\n\nE-mail: $email\n\nTelefone: $fone\n\nMensagem: $mensagem\n\nRegião que deseja atuar: $estado";
 
-require_once("phpmailer/class.phpmailer.php");
 
-define('GUSER', 'igntemplates@gmail.com');	// <-- Insira aqui o seu GMail
-define('GPWD', '258749632400');		// <-- Insira aqui a senha do seu GMail
+require_once("phpmailer/PHPMailerAutoload.php");
+
+define('GUSER', 'igntemplates@gmail.com');  // <-- Insira aqui o seu GMail
+define('GPWD', '258749632400');    // <-- Insira aqui a senha do seu GMail
 
 function smtpmailer($para, $de, $de_nome, $assunto, $corpo) { 
-	global $error;
-	$mail = new PHPMailer();
-	$mail->IsSMTP();		// Ativar SMTP
-	$mail->SMTPDebug = 0;		// Debugar: 1 = erros e mensagens, 2 = mensagens apenas
-	$mail->SMTPAuth = true;		// Autenticação ativada
-	$mail->SMTPSecure = 'ssl';	// SSL REQUERIDO pelo GMail
-	$mail->Host = 'smtp.gmail.com';	// SMTP utilizado
-	$mail->Port = 587;  		// A porta 587 deverá estar aberta em seu servidor
-	$mail->Username = GUSER;
-	$mail->Password = GPWD;
-	$mail->SetFrom($de, $de_nome);
-	$mail->Subject = $assunto;
-	$mail->Body = $corpo;
-	$mail->AddAddress($para);
-	if(!$mail->Send()) {
-		$error = 'Mail error: '.$mail->ErrorInfo; 
-		return false;
-	} else {
-		$error = 'Mensagem enviada!';
-		return true;
-	}
+  global $error;
+  $mail = new PHPMailer();
+  $mail->IsSMTP();    // Ativar SMTP
+  $mail->SMTPDebug = 0;   // Debugar: 1 = erros e mensagens, 2 = mensagens apenas
+  $mail->SMTPAuth = true;   // Autenticação ativada
+  $mail->SMTPSecure = 'tls';  // tsl REQUERIDO pelo GMail
+  $mail->Host = 'smtp.gmail.com'; // SMTP utilizado
+  $mail->Port = 587;      // A porta 587 deverá estar aberta em seu servidor
+  $mail->Username = GUSER;
+  $mail->Password = GPWD;
+  $mail->SetFrom($de, $de_nome);
+  $mail->Subject = $assunto;
+  $mail->Body = $corpo;
+  $mail->AddAddress($para);
+  if(!$mail->Send()) {
+    $error = 'Mail error: '.$mail->ErrorInfo; 
+    echo $error;
+    return false;
+  } else {
+    $error = 'Mensagem enviada!';
+    return true;
+  }
 }
 
-// Insira abaixo o email que irá receber a mensagem, o email que irá enviar (o mesmo da variável GUSER), 
-o nome do email que envia a mensagem, o Assunto da mensagem e por último a variável com o corpo do email.
+smtpmailer('www.igntemplates@gmail.com', 'igntemplates@gmail.com', 'Enviador', 'Assunto', $Vai);
 
- if (smtpmailer('recebedor@dominio.com.br', 'enviador@gmail.com', 'Nome do Enviador', 'Assunto do Email', $Vai)) {
+Header("location:http://www.obrigado.com.br"); // Redireciona para uma página de obrigado.
 
-	Header("location:http://www.dominio.com.br/obrigado.html"); // Redireciona para uma página de obrigado.
-
-}
-if (!empty($error)) echo $error;
-?>
+?> 
